b42abb4b8bf112de9a525dbfbb3d7ae05b8e3be2
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-11-25 21:37:01 +0100

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,19 @@ import { Contacts } from "./Contacts/Contacts";
 import { useState, useEffect } from "react";
 
 
-
-const LS_KEY = 'contacts';
-
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
+    return (JSON.parse(localStorage.getItem('contacts')) || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]);
   });
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const handleChange = evt => {
@@ -36,9 +38,9 @@ export const App = () => {
   
 
   const addContact = (values) => {
-    const prevContacts = contacts.find(
+    if (contacts.find(
       contact => contact.name.toLowerCase() === values.name.toLowerCase());
-    if (prevContacts) {
+    {
       alert(`${values.name} is already in contacts.`);
       return;
     };
